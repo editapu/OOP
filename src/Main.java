f8c49e5698965ef09853096650a732158c10d881
editapu <pucinskaitee@gmail.com>
2023-06-16 20:30:51 +0300

    užduotis su knygom

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
@@ -9,20 +10,33 @@ public class Main {
         ArrayList<Book> library = new ArrayList<>();
 
         Book knyga = new Book();
-
-       // knyga.title = "Panama labai grazi";
         knyga.setTitle("Panama labai grazi");
-        //knyga.pages = 20;
         knyga.setPages(20);
-        //knyga.releaseYear = 2000;
         knyga.setReleaseYear(2000);
 
-        library.add("Panama labai grazi");
-        library.add(20);
-        library.add(2000);
+        Book knyga2 = new Book();
+        knyga2.setTitle("Kakė Makė");
+        knyga2.setPages(10);
+        knyga2.setReleaseYear(19999);
+
+        Book knyga3 = new Book();
+        knyga3.setTitle("Laimė yra lapė");
+        knyga3.setPages(15);
+        knyga3.setReleaseYear(2023);
+
+        Book book1 = new Book("pavadinimas",10,10);
+        Book book2 = new Book ("pavadinimas", 10, 19999);
+        Book book3 = new Book("pavadinimas", 15, 2023);
+
+        library.add(knyga);
+        library.add(knyga2);
+        library.add(knyga3);
+        library.add(book1);
+        library.add(book2);
+        library.add(book3);
 
         for (int i = 0; i < library.size(); i++) {
-
+            System.out.println(library.get(i).getTitle() + " " + library.get(i).getPages() + " " + library.get(i).getReleaseYear() );
         }
 
 
